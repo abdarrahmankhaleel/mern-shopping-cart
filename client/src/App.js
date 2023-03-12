fe80c74634a9d178c359c3c8a47966b8fce57f2d
abdarraman khaleel <email@example.com>
2023-03-12 22:38:17 +0300

    adding on filter comp

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,60 @@ import data from './/data/data.json';
 import Filter from './components/Filter/Filter';
 function App() {
   const [products,setProducts]=useState(data);
-  console.log(products);
+  const [sort,setsort]=useState("");
+  const [filter,setfilter]=useState("");
+ 
+const filterBySize=(e)=>{
+  console.log(e.target.value);
+if (e.target.value=="all") {
+ setProducts(data);
+}else{
+  let productsClone=data;
+
+  let newProducts=productsClone.filter(p=>p.size.indexOf(e.target.value)!=-1);
+
+  setProducts(newProducts);
+
+}
+
+}
+
+
+
+
+const sorting=(e)=>{
+  let typeOfSorting=e.target.value;
+  if(typeOfSorting==""){
+    setProducts(data);
+  }else{
+    let productsClone=data;
+  
+    let newProducts=productsClone.sort(function(first,second){
+      if(typeOfSorting=="lowest"){
+        return first.price - second.price
+      }else if (typeOfSorting=="hist"){
+        return  second.price-first.price 
+      }else{
+        return  first.id<second.id ? 1 : -1
+      }
+    });
+  
+    console.log(typeOfSorting);
+  
+    setProducts(newProducts);
+  }
+
+}
+
   return (
     <div className="App">
       <Header/>
       <main className='wrapper'>
       <Products products={products}/>
-       <Filter/>
+       <Filter 
+       filterBySize={filterBySize}
+       sorting={sorting}
+       />
       </main>
 
      <Footer/>
--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import '../../css/Filter/Filter.css'
-const Filter = () => {
+const Filter = (prop) => {
   return (
     <div className='filter-wrapper'>
         <h2>Filter</h2>
         <p>Number All Products :4 Products</p>
         <div className='filter-size'>
             <label>Size</label>
-            <select>
+            <select onChange={prop.filterBySize}>
+                <option value="all">All</option>
                 <option value="S">S</option>
                 <option value="M">M</option>
                 <option value="XL">XL</option>
                 <option value="L">L</option>
                 <option value="XXL">XXL</option>
-                <option value="XXL">XXL</option>
             </select>
         </div>
         <div className='filter-order'>
             <label>order</label>
-            <select>
+            <select onChange={prop.sorting}>
+            <option value="">hojv</option>
                 <option value="latest">Latest</option>
                 <option value="lowest">lowest</option>
                 <option value="hist">Hightest</option>
